be5689bf94f1279691d7afc07c26c1f37d423a33
Daniel Sokic <44309771+danielsokic@users.noreply.github.com>
2025-02-14 19:12:29 -0800

    Added score

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,30 +3,17 @@
 
     public class BallController : MonoBehaviour
     {
-        [SerializeField] private float force =1f;
+        [SerializeField] private float force = 1f;
         [SerializeField] private Transform ballAnchor;
         [SerializeField] private Transform launchIndicator;
         private bool isBallLaunched;
       
-        private InputManager inputManager;
+        [SerializeField] private InputManager inputManager;
         private Rigidbody ballRB;
 
         void Start(){
             ballRB = GetComponent<Rigidbody>();
-            if (inputManager == null)
-        {
-            inputManager = FindObjectOfType<InputManager>();
-        }
-
-        // Check if inputManager was found before using it
-        if (inputManager != null)
-        {
             inputManager.OnSpacePressed.AddListener(LaunchBall);
-        }
-        else
-        {
-            Debug.LogError("InputManager not found! Make sure it's in the scene.");
-        }
             transform.parent = ballAnchor;
             transform.localPosition = Vector3.zero;     
             ballRB.isKinematic = true;       
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+
+    private void Start(){
+      pins = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None);
+        foreach(FallTrigger pin in pins){
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }    
+
+    private void IncrementScore(){
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
